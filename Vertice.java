31649aa58fdad5931e820a5b79bb134b11c75cd0
wanessa-maraujo <wanessamartinsdearaujo@gmail.com>
2023-09-20 21:53:06 -0300

    Adicionei classe vertice com get e set

---
+++ Vertice.java
@@ -0,0 +1,16 @@
+public class Vertice {
+
+  String nomeVertice;
+
+  public Vertice(String nomeVertice) {
+    this.nomeVertice = nomeVertice;
+  }
+
+  public String getNomeVertice() {
+    return nomeVertice;
+  }
+
+  public String setNomeVertice() {
+    return nomeVertice;
+  }
+}
